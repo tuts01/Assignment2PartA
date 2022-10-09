994e8313dc290cc7fbd6072c5447fe3b5b14f6a4
Tristan <19836511@student.curtin.edu.au>
2022-10-09 15:46:24 +0800

    Created Urls - Interface containing constants for query URLS

---
+++ app/src/main/java/com/tutungis/assignment2parta/Urls.java
@@ -0,0 +1,6 @@
+package com.tutungis.assignment2parta;
+
+public interface Urls {
+    String USERS_URL = "http://jsonplaceholder.typicode.com/users";
+    String POSTS_URL = "http://jsonplaceholder.typicode.com/posts";
+}
