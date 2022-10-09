f535e6162af9cd3d33c017ead68932446c665a9f
Tristan <19836511@student.curtin.edu.au>
2022-10-09 15:43:18 +0800

    Defined custom HTTP response exception - thrown when HTTP code indicates an error code

---
+++ app/src/main/java/com/tutungis/assignment2parta/HttpResponseException.java
@@ -0,0 +1,11 @@
+package com.tutungis.assignment2parta;
+
+import java.io.IOException;
+
+public class HttpResponseException extends IOException
+{
+    public HttpResponseException(String str)
+    {
+        super(str);
+    }
+}
