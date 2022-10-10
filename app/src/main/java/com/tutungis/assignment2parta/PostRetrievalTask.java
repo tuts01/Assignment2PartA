943addce5812e3ad7bbde486f551721ab2967fe8
Tristan S. Tutungis <19836511@student.curtin.edu.au>
2022-10-10 12:57:24 +0800

    Created PostRetrievalTask

---
+++ app/src/main/java/com/tutungis/assignment2parta/PostRetrievalTask.java
@@ -0,0 +1,13 @@
+package com.tutungis.assignment2parta;
+
+import java.util.ArrayList;
+import java.util.concurrent.Callable;
+
+public class PostRetrievalTask implements Callable<ArrayList<Post>>, Urls
+{
+    public ArrayList<Post> call()
+    {
+    
+        return null;//Stub!
+    }
+}
