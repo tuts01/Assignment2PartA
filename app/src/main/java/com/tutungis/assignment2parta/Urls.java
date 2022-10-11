35dc4eb7b8ba9f280cd7c876fd2ae5ad88fee8a5
Tristan <19836511@student.curtin.edu.au>
2022-10-11 08:38:15 +0800

    Added https:// to the URL strings. Will probably deprecate this class soon.

--- app/src/main/java/com/tutungis/assignment2parta/Urls.java
+++ app/src/main/java/com/tutungis/assignment2parta/Urls.java
@@ -6,9 +6,9 @@ package com.tutungis.assignment2parta;
  * @interface       Urls
  * @author          Tristan S. Tutungis
  * @date_created    9/10/2022
- * @last_modified   10/10/2022 10:40
+ * @last_modified   10/10/2022 23:53
  */
 public interface Urls {
-    String USERS_URL = "http://jsonplaceholder.typicode.com/users";
-    String POSTS_URL = "http://jsonplaceholder.typicode.com/posts";
+    String USERS_URL = "https://jsonplaceholder.typicode.com/users";
+    String POSTS_URL = "https://jsonplaceholder.typicode.com/posts";
 }
